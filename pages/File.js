5db6a9b601af3a3ddd08eec18d47bc691d1fa09d
Hamza65523 <102131334+Hamza65523@users.noreply.github.com>
2022-09-21 22:00:40 +0500

    initial commit

---
+++ pages/File.js
@@ -0,0 +1,19 @@
+ let data = {
+        id:'0',
+        children:[
+            {
+                id:'0-0',
+                children:[
+                    {
+                        id:'0-0-0',
+                        children:[
+                            {
+                                id:'0-0-0-0'
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+export default data;
